c0283abec4fa3ab573297c9c4c257e8771537913
Ashlyn Smith <ashlyn.marie.smith2@gmail.com>
2020-10-23 16:56:37 -0400

    added feilds to workout.js

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -2,28 +2,46 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+    required: "Day is required"
+  },
+  exercises: [
+   { 
     name: {
         type: String,
         trim: true,
         required: "Name is required"
       },
-      size: {
-        type: Number,
-        required: true
-      },
-      price: {
-          type: String,
-          trim: true,
-          required: "Price is required"
-      },
-      ingredients: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: "Ingredient"
-        }
+
+      type: {
+        type: String,
+        trim: true,
+        required: "Type is required"
+    },      
+    weight: {
+      type: Number,
+      required: false
+    },
+    sets: {
+      type: Number,
+      required: false
+    },
+    reps: {
+      type: Number,
+      required: false
+    },
+    duration: {
+      type: Number,
+      required: "Duration is required"
+    },
+    distance: {
+      type: Number,
+      required: true
+    },
+    }
       ]
-    
-    
     });
   
   const Workout = mongoose.model("Workout", WorkoutSchema);
