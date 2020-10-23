092f321341b53760fe87971932640cb29082005a
Ashlyn Smith <ashlyn.marie.smith2@gmail.com>
2020-10-23 16:59:37 -0400

    updated feilds to workout.js

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -5,45 +5,54 @@ const WorkoutSchema = new Schema({
   day: {
     type: Date,
     default: Date.now,
-    required: "Day is required"
+    required: "Day is required",
   },
   exercises: [
-   { 
-    name: {
+    {
+      name: {
         type: String,
         trim: true,
-        required: "Name is required"
+        required: "Name is required",
       },
 
       type: {
         type: String,
         trim: true,
-        required: "Type is required"
-    },      
-    weight: {
-      type: Number,
-      required: false
+        required: "Type is required",
+      },
+      weight: {
+        type: Number,
+        required: false,
+      },
+      sets: {
+        type: Number,
+        required: false,
+      },
+      reps: {
+        type: Number,
+        required: false,
+      },
+      duration: {
+        type: Number,
+        required: "Duration is required",
+      },
+      distance: {
+        type: Number,
+        required: false,
+      },
     },
-    sets: {
-      type: Number,
-      required: false
-    },
-    reps: {
-      type: Number,
-      required: false
-    },
-    duration: {
-      type: Number,
-      required: "Duration is required"
-    },
-    distance: {
-      type: Number,
-      required: true
-    },
-    }
-      ]
-    });
-  
-  const Workout = mongoose.model("Workout", WorkoutSchema);
-  
-  module.exports = Workout;
+  ],
+  toJSON:{
+    virtuals: true
+  }
+});
+
+WorkoutSchema.virtual("totalDuration").get(function (){
+  return.this.exercises.reduce((total, exercises)=>{
+    return total + exercises.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
